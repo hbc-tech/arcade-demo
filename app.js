3e8bde87bf3d205c7759f74ab55202cc27d74cec
fiftyfactorauthentication <powella@gilt.com>
2017-10-30 21:00:08 -0400

    updating demo for recent work

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 
 const { Arcade } = require('@hbc-tech/arcade-app');
 const header = require('@hbc-tech/arcade-header');
+const footer = require('@hbc-tech/arcade-footer');
 
 const arcade = new Arcade();
 
 header(arcade);
+footer(arcade);
+
+arcade.app.use(async (ctx, next) => {
+  ctx.arcade.head.links.push({
+    rel: 'stylesheet',
+    href: '/assets/css/framework_commons_saks.css'
+  });
+  await next();
+});
 
 arcade.get('/', (ctx) => {
   ctx.arcade.render('Hello World');
